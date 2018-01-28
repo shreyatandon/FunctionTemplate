b210cc738be2b385cc4707668a554eff53efb2e9
shreyatandon <shreyatandon91@gmail.com>
2018-01-28 16:12:05 +0530

    Added Two Different Template Paramateres

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+//Single "K" means only only one template parameters
 template<typename K>
 K sum(K x, K y)
 {
@@ -10,10 +12,19 @@ K sum(K x, K y)
 }
 
 
+template<typename K1, typename K2>
+K1 sum(K1 x, K2 y)
+{
+	K1 z = x + y;
+	return z;
+}
+
 int main()
 {
 	cout << "Ints Ka Sum   " << sum(1, 2) << endl;
 	cout << "Doubles Ka Sum   " << sum(1.5, 2.2) << endl;
+	cout << "Double and Int Ka Sum   " << sum(1.5, 2) << endl;
+
 	getchar();
 
 }
